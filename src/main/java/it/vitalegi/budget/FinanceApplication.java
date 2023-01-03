5360e4f01af884e934fc2cddb96437b36c7920d3
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-01-03 23:04:22 +0100

    init

---
+++ src/main/java/it/vitalegi/budget/FinanceApplication.java
@@ -0,0 +1,13 @@
+package it.vitalegi.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApplication.class, args);
+	}
+
+}
