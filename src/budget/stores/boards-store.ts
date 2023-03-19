43bebf21e347cf1a3ac2864fc6ffa5d7a2226659
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-03-19 16:30:35 +0100

    hide sync if sidebar

--- src/budget/stores/boards-store.ts
+++ src/budget/stores/boards-store.ts
@@ -1,15 +1,12 @@
 import { defineStore } from 'pinia';
 import boardService from 'src/budget/integrations/BoardService';
 import Board from 'src/budget/models/Board';
-import spinner from 'src/utils/Spinner';
 import { ref } from 'vue';
 
 export const useBoardsStore = defineStore('boards', () => {
   const boards = ref(new Array<Board>());
   const update = async (): Promise<void> => {
-    await spinner.sync(async () => {
-      boards.value = await boardService.getBoards();
-    });
+    boards.value = await boardService.getBoards();
   };
   update();
   return { boards, update };
