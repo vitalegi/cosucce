1e7962d7715ec6d873f3ef187eb46e23ae1a14e6
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-06-04 18:59:13 +0200

    fix spring security

--- src/main/java/it/vitalegi/config/WebSecurityConfiguration.java
+++ src/main/java/it/vitalegi/config/WebSecurityConfiguration.java
@@ -1,25 +1,19 @@
 package it.vitalegi.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -34,19 +28,17 @@ public class WebSecurityConfiguration {
     Boolean allowCredentials;
     @Value("${security.cors.allowedHeaders}")
     List<String> allowedHeaders;
-    @Autowired
-    private RestTemplateBuilder restTemplateBuilder;
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> //
-                requests.requestMatchers(HttpMethod.OPTIONS).permitAll() //
-                        .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/v3/api" + "-docs.yaml", "/swagger-ui/*")
-                        .permitAll() //
-                        .anyRequest().authenticated());
-        return http.build();
+        return http.authorizeHttpRequests((requests) -> requests //
+                                                                 .requestMatchers("/v3/api-docs", "/v3/api-docs/**",
+                                                                         "/v3/api-docs.yaml", "/swagger-ui/*")
+                                                                 .permitAll() //
+                                                                 .anyRequest().authenticated() //
+
+                   )//
+                   .cors(withDefaults()).oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())).build();
     }
 
     @Bean
@@ -64,11 +56,5 @@ public class WebSecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    Converter<Jwt, AbstractAuthenticationToken> grantedAuthoritiesExtractor() {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new GrantedAuthoritiesExtractor());
-        return jwtAuthenticationConverter;
     }
 }
