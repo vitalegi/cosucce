9f77f437879b00a225f90d40ca3163ec65adc5ba
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2024-09-29 11:27:42 +0200

    remove unused content

--- monefy/src/router/routes.ts
+++ monefy/src/router/routes.ts
@@ -5,11 +5,7 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      {
-        path: '/monefy',
-        component: () => import('pages/MonefyHomePage.vue'),
-      },
+      { path: '', component: () => import('pages/MonefyHomePage.vue') },
     ],
   },
 
