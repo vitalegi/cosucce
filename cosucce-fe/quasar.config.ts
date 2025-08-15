c734bf54db0bf6e7d8816b35d16b4b58f3a92677
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2025-08-15 20:50:10 +0200

    enable quasar notify plugin

--- cosucce-fe/quasar.config.ts
+++ cosucce-fe/quasar.config.ts
@@ -85,7 +85,9 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      config: {
+        notify: {},
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -98,7 +100,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify'],
     },
 
     // animations: 'all', // --- includes all animations
